02acfffbf1bee788ae2942f25a5de6d6a8fb30ee
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-09 05:03:08 +0800

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
+import { createEvent, createStore, sample } from 'effector'
+import { using, h } from 'forest'
 import './style.css'
+
+const app = document.querySelector<HTMLElement>('#app')!
+
+using(app, () => {
+  const $title = createStore('hello')
+
+  const changed = createEvent<Event>()
+  const submited = createEvent<MouseEvent>()
+
+  $title.on(changed, (state, event) => {
+    return (event.target as HTMLInputElement).value
+  })
+
+  sample({
+    source: $title,
+    clock: changed,
+    fn: (title) => console.log('title:', title)
+  })
+
+  sample({
+    source: $title,
+    clock: submited,
+    fn: (title) => alert(title)
+  })
+
+  h('div', () => {
+    h('h1', { text: $title })
+    h('input', {
+      attr: { type: 'text', value: $title },
+      handler: { input: changed }
+    })
+    h('button', {
+      text: 'Submit',
+      handler: { click: submited },
+      visible: $title.map((title) => title.length >= 10)
+    })
+  })
+})
